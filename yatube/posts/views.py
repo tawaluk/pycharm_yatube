e4c1b6d28b8ea3928654d64260e2f8d610603a91
tawaluk <tawaluk@yandex.ru>
2023-02-05 23:24:49 +0300

    победил и осознал URL и views

---
+++ yatube/posts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('main page')
+
+def group_posts(request, slug):
+    return HttpResponse(f'Group {slug}')

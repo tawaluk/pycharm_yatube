ef3b4cba85de402b144cc9c42d52a38f5740795e
tawaluk <tawaluk@yandex.ru>
2023-02-07 16:42:32 +0300

    добавил в вьюшку словари и сделал вывод title из словаря, а не из шаблона

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,8 +4,17 @@ from django.http import HttpResponse
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    title = 'Это главная страница проекта Yatube'
+    var_title = {
+        'title' : title,
+    }
+    return render(request, template, var_title)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Group {slug}')
+    template_2 = 'posts/group_list.html'
+    title_2 = 'Здесь будет информация о группах проекта Yatube'
+    var_title_2 = {
+        'title_2' : title_2,
+    }
+    return render(request, template_2, var_title_2)
